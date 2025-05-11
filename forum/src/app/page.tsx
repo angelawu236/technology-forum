9b7e433086a82cd4d99b054f5a48128c88bfeaff
angelawu236 <anw041@ucsd.edu>
2025-05-11 13:45:55 -0700

    testing push2

--- forum/src/app/page.tsx
+++ forum/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <PostSection></PostSection>
       </div>
       <div className = "basis-2/9">
-        Topics
+        TOPICS
       </div>
     </div>
   );
